660548fe4ce5e33e96ec6a382ee6b2a7750443f7
tery <vladislavutkin92@gmail.com>
2019-02-28 14:59:19 +0300

    исправлено решение задачи

--- src/js/app.js
+++ src/js/app.js
@@ -3,18 +3,19 @@
 let costs = [200, 550, 4000, 23, 58, 5000, 485, 711];
 const bonus = 5;
 const limBuy = 10000;
-
-window.onload = function () {
+let index;
+let sum = 0;
+function sumOfValue() {
     console.log(`Let's rock! `);
-    for (let index of costs) {
-        let sum = index + index + index;
-        if (sum > limBuy) {
-            let buy = sum * (bonus * 0.01);
-            let discount = sum - buy;
-            console.log(`Цена ваших покупок: ${discount}руб.\n Ваша скидка составила: ${buy}руб.\n\n\n`);
-        } else {
-            console.log(`Цена ваших покупок: ${sum}руб.\n\n\n`);
-        }
+    for (index of costs) {
+        sum = sum + index;
     }
-};
-
+    if (sum > limBuy) {
+        let buy = sum * (bonus * 0.01);
+        let discount = sum + buy;
+        console.log(`Цена ваших покупок: ${Math.round(discount)}.\n Ваш бонус составил: ${Math.round(buy)}.\n\n\n`);
+    } else {
+        console.log(`Цена ваших покупок: ${Math.round(sum)}.\n\n\n`);
+    }
+}
+sumOfValue();
